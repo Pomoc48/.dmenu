deb702d368ed354bd32c924d6c8a9daeb9844cbc
Pomoc48 <lukawski.mikolaj@gmail.com>
2022-04-22 15:41:28 +0200

    Make dmenu window floating

--- config.h
+++ config.h
@@ -2,6 +2,8 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 750;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Hack:pixelsize=16:antialias=true"
@@ -14,7 +16,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 20;
 
 static unsigned int lineheight = 24;
 static unsigned int min_lineheight = 24;
@@ -23,3 +25,6 @@ static unsigned int min_lineheight = 24;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 2;
